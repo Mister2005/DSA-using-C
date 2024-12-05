75f46c127148ec77ff885689564a37a8202fcc50
Varun Gupta <varunygupta123@gmail.com>
2024-12-05 16:17:50 +0530

    Done some minor changes

--- Practice Questions/Practical_exam_soln.c
+++ Practice Questions/Practical_exam_soln.c
@@ -1,111 +1,88 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#define MAX 100
+#include <stdio.h>
+#include <stdlib.h>
 
-int array[MAX];
-int top1 = -1;
-int top2 = MAX;
+#define MAX_SIZE 100
 
-void pushUndo(int value) {
-    if (top1 + 1 == top2) {
-        printf("Stack Overflow! Cannot push into Undo stack.\n");
-    } else {
-        array[++top1] = value;
-        printf("Pushed %d into Undo stack.\n", value);
-    }
-}
-
-void pushRedo(int value) {
-    if (top1 + 1 == top2) {
-        printf("Stack Overflow! Cannot push into Redo stack.\n");
-    } else {
-        array[--top2] = value;
-        printf("Pushed %d into Redo stack.\n", value);
-    }
-}
-
-int popUndo() {
-    if (top1 == -1) {
-        printf("Undo stack is empty!\n");
-        return -1;
-    } else {
-        int value = array[top1--];
-        printf("Popped %d from Undo stack.\n", value);
-        return value;
-    }
-}
-
-int popRedo() {
-    if (top2 == MAX) {
-        printf("Redo stack is empty!\n");
-        return -1;
-    } else {
-        int value = array[top2++];
-        printf("Popped %d from Redo stack.\n", value);
-        return value;
-    }
-}
-
-void peekUndo() {
-    if (top1 == -1) {
-        printf("Undo stack is empty!\n");
-    } else {
-        printf("Top of Undo stack: %d\n", array[top1]);
-    }
-}
-
-void peekRedo() {
-    if (top2 == MAX) {
-        printf("Redo stack is empty!\n");
-    } else {
-        printf("Top of Redo stack: %d\n", array[top2]);
-    }
-}
-
-int main() 
+typedef struct 
 {
-    int choice, stack, value;
-    while (1) {
-        printf("\nMenu:\n");
-        printf("1. Push into Undo stack\n");
-        printf("2. Push into Redo stack\n");
-        printf("3. Pop from Undo stack\n");
-        printf("4. Pop from Redo stack\n");
-        printf("5. Peek Undo stack\n");
-        printf("6. Peek Redo stack\n");
-        printf("7. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+    int array[MAX_SIZE];
+    int top1;
+    int top2;
+}TwoStacks;
 
-        switch (choice) {
-            case 1:
-                printf("Enter value to push into Undo stack: ");
-                scanf("%d", &value);
-                pushUndo(value);
-                break;
-            case 2:
-                printf("Enter value to push into Redo stack: ");
-                scanf("%d", &value);
-                pushRedo(value);
-                break;
-            case 3:
-                popUndo();
-                break;
-            case 4:
-                popRedo();
-                break;
-            case 5:
-                peekUndo();
-                break;
-            case 6:
-                peekRedo();
-                break;
-            case 7:
-                printf("Exiting...\n");
-                return 0;
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
+void initializeStacks(TwoStacks *stacks) 
+{
+    stacks->top1 = -1;
+    stacks->top2 = MAX_SIZE;
+}
+
+void pushStack1(TwoStacks *stacks, int value) 
+{
+    if (stacks->top1 + 1 == stacks->top2) {
+        printf("Stack Overflow! Cannot push to Stack 1.\n");
+        return;
     }
+    stacks->array[++(stacks->top1)] = value;
+}
+
+void pushStack2(TwoStacks *stacks, int value) {
+    if (stacks->top1 + 1 == stacks->top2) {
+        printf("Stack Overflow! Cannot push to Stack 2.\n");
+        return;
+    }
+    stacks->array[--(stacks->top2)] = value;
+}
+
+int popStack1(TwoStacks *stacks) {
+    if (stacks->top1 == -1) {
+        printf("Stack Underflow! Stack 1 is empty.\n");
+        return -1;
+    }
+    return stacks->array[(stacks->top1)--];
+}
+
+int popStack2(TwoStacks *stacks) {
+    if (stacks->top2 == MAX_SIZE) {
+        printf("Stack Underflow! Stack 2 is empty.\n");
+        return -1;
+    }
+    return stacks->array[(stacks->top2)++];
+}
+
+int peekStack1(TwoStacks *stacks) {
+    if (stacks->top1 == -1) {
+        printf("Stack 1 is empty.\n");
+        return -1;
+    }
+    return stacks->array[stacks->top1];
+}
+
+int peekStack2(TwoStacks *stacks) {
+    if (stacks->top2 == MAX_SIZE) {
+        printf("Stack 2 is empty.\n");
+        return -1;
+    }
+    return stacks->array[stacks->top2];
+}
+
+int main() {
+    TwoStacks stacks;
+    initializeStacks(&stacks);
+    pushStack1(&stacks, 10);
+    pushStack1(&stacks, 20);
+    pushStack1(&stacks, 30);
+    
+    pushStack2(&stacks, 100);
+    pushStack2(&stacks, 200);
+    pushStack2(&stacks, 300);
+
+    printf("Top of Stack 1: %d\n", peekStack1(&stacks));
+    printf("Top of Stack 2: %d\n", peekStack2(&stacks));
+    
+    printf("Popped from Stack 1: %d\n", popStack1(&stacks));
+    printf("Popped from Stack 2: %d\n", popStack2(&stacks));
+
+    pushStack1(&stacks, 40);
+    printf("New top of Stack 1: %d\n", peekStack1(&stacks));
+    return 0;
 }
